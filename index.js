ef033ce6db6f9acc2b5721b7435db1bd58ce6095
kokrok <kokr7130@gmail.com>
2022-08-14 13:44:26 +0900

    layout.css변경

---
+++ index.js
@@ -0,0 +1,16 @@
+function updateMainFrameImg(){
+    var selectedID = document.getElementById("imgCat");
+    selectedID.innerHTML="<img src='점프 고양이.jpg' alt='이미지 불러오기 실패'>";
+}
+function updateMainFrameImg_smile(){
+    var selectedID = document.getElementById("imgCat");
+    selectedID.innerHTML="<img src='웃는 고양이.jpg' alt='이미지 불러오기 실패'>";
+}
+function updateMainFrameImg_Flying(){
+    var selectedID = document.getElementById("imgCat");
+    selectedID.innerHTML="<img src='점프 고양이.jpg' alt='이미지 불러오기 실패'>";
+}
+function updateMainFrameImg_sleep(){
+    var selectedID = document.getElementById("imgCat");
+    selectedID.innerHTML="<img src='잠든 고양이.jpg' alt='이미지 불러오기 실패'>";
+}
