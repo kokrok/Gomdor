fee182f403e42ce22328a009ea59a3e851b1cd2b
kokrok <kokr7130@gmail.com>
2023-08-23 00:26:35 +0900

    Update 소수박스.js

--- 소수박스.js
+++ 소수박스.js
@@ -6,18 +6,40 @@ canvas.width = window. innerWidth * 0.85;
 canvas.height = window.innerHeight * 0.6;
 
 /*Make Object*/
-
+class graph {
+  graph_left_margin = 30;
+  graph_right_margin = 30;
+  graph_top_margin = 30;
+  graph_bottom_margin = 30;
+  graph_width_white = canvas.width - graph_left_margin - graph_right_margin;
+  graph_height_white = canvas.height - graph_top_margin - graph_bottom_margin;
+  // constructor(height, width) {
+  //   this.height = height;
+  //   this.width = width;
+  // }
+  draw(){
+    x = graph_left_margin;
+    y = graph_top_margin;
+    width = graph_width_white;
+    height = graph_height_white;
+    ctx.beginPath();
+    ctx.fillStyle = "#99999";
+    ctx.fillRect(x, y, width, height);
+    ctx.closePath();
   }
-graph_start_x = 20;
-graph_end_x = canvas.width - 20;
-graph_width = graph_end_x - graph_start_x;
-graph_start_y = 20;
-graph_end_y = canvas.height - 20;
-graph_height = graph_end_y - graph_start_y;
+}
+
+
+graph_left_limit = 20;
+graph_right_limit = canvas.width - 20;
+graph_width = graph_right_limit - graph_left_limit;
+graph_top_limit = 20;
+graph_bottom_limit = canvas.height - 20;
+graph_height = graph_bottom_limit - graph_top_limit;
 
 ctx.beginPath();
-ctx.fillStyle = "white";
-ctx.fillRect(graph_start_x, graph_start_y, graph_width, graph_height);
+ctx.fillStyle = "#338833";
+ctx.fillRect(graph_left_limit, graph_bottom_limit, graph_width, -graph_height);
 ctx.closePath();
 
 
@@ -38,7 +60,12 @@ Xgrid_end = canvas.width-20;
 Ygrid_start = 20;
 Ygrid_end = canvas.height - 20;
 
-ctx.beginPath();
-ctx.moveTo(20,canvas.height - 20)
-ctx.lineTo(20,20);
-ctx.stroke();
+for(ix = 0; ix<Xdata_count; ix ++){
+  ctx.beginPath();
+  ctx.moveTo(graph_left_limit+ix*graph_bottom_limit);
+  ctx.lineTo(20,20);
+  ctx.stroke();
+
+}
+const g = new graph();
+g.draw();
