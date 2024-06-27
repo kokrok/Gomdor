aa2f89878d1100e4cdcf149ce7d641f96ce92a66
kokrok <kokr7130@gmail.com>
2024-06-27 21:36:00 +0900

    git다운로드해서 복붙..

--- 소수박스.js
+++ 소수박스.js
@@ -11,6 +11,16 @@ var ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth * 0.85;
 canvas.height = window.innerHeight * 0.6;
 
+
+//let option_gridSpace = "VALUEDEPEND";
+let option_gridSpace = "SPACEDEPEND";
+let value1 = document.getElementById('text1').value; //시작정수
+let value2 = document.getElementById('text2').value; //끝정수
+
+console.log(value1);
+console.log(value2);
+
+
 let grid_short_bar_space = 25;
 let grid_short_bar_length = 5;
 let x_grid_short_bar_space = 1 * grid_short_bar_space;
@@ -20,8 +30,6 @@ let origin_X = 20;
 let origin_Y = canvas.height - 20;
 
 /*Make Object*/
-<<<<<<< HEAD
-=======
 function clearCanvas(){
   ctx.beginPath();
   ctx.fillStyle = "white";
@@ -29,7 +37,7 @@ function clearCanvas(){
   ctx.closePath();
 }
 
-function drawGraph(){
+function drawGraph(value1, value2){
   let graphLeftMargin = 20;
   let graphRightMargin = 20;
   let graphTopMargin = 20;
@@ -41,59 +49,148 @@ function drawGraph(){
   let graph_start_y = graphTopMargin;
   let graph_end_y = canvas.height - graphBottomMargin;
   let graph_height = graph_end_y - graph_start_y;
->>>>>>> parent of 9e03b87 (소수 박스 첫 완성!)
 
 
-let graph_start_x = 20;
-let graph_end_x = canvas.width - 20;
-let graph_width = graph_end_x - graph_start_x;
-let graph_start_y = 20;
-let graph_end_y = canvas.height - 20;
-let graph_height = graph_end_y - graph_start_y;
+  
+  // constructor(height, width) {
+  //   this.height = height;
+  //   this.width = width;
+  // }
+  // draw(){
+  //   ctx.beginPath();
+  //   ctx.fillStyle = "white";
+  //   ctx.fillRect(graph_start_x, graph_start_y, graph_width, graph_height);
+  //   ctx.closePath();
+  // }
 
-ctx.beginPath();
-ctx.fillStyle = "white";
-ctx.fillRect(graph_start_x, graph_start_y, graph_width, graph_height);
-ctx.closePath();
+  //// canvas 배경 색
+  ctx.beginPath();
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.closePath();
 
-  ////눈금그리기
-  //x 축 위 세로 눈금
-  for(let i = 0; i<100; i++){
-    ctx.beginPath();
-    ctx.strokeStyle="black";
-    ctx.moveTo(origin_X+(i-30)*x_grid_short_bar_space,origin_Y-grid_short_bar_length);
-    ctx.lineTo(origin_X+(i-30)*y_grid_short_bar_space,origin_Y+grid_short_bar_length);
-    ctx.stroke();
-    ctx.closePath();
+  //origin Spot 그리기
+  ctx.beginPath();
+  ctx.arc(origin_X, origin_Y, 3, 0, Math.PI*2);
+  ctx.fillStyle="brack";
+  ctx.fill();
+  ctx.closePath();
+
+  ////x,y axis 그리기
+  // X 축 그리기
+  ctx.beginPath();
+  ctx.strokeStyle="black";
+  ctx.moveTo(0,origin_Y);
+  ctx.lineTo(canvas.width,origin_Y);
+  ctx.stroke();
+  ctx.closePath();
+  // Y 축 그리기
+  ctx.beginPath();
+  ctx.strokeStyle="black";
+  ctx.moveTo(origin_X,canvas.height);
+  ctx.lineTo(origin_X,0);
+  ctx.stroke();
+  ctx.closePath();
+
+  if(option_gridSpace === "VALUEDEPEND"){
+      ////눈금그리기
+    //x 축 위 세로 눈금
+    for(let i = 0; i<100; i++){
+      ctx.beginPath();
+      ctx.strokeStyle="black";
+      ctx.moveTo(origin_X+(i-30)*x_grid_short_bar_space,origin_Y-grid_short_bar_length);
+      ctx.lineTo(origin_X+(i-30)*x_grid_short_bar_space,origin_Y+grid_short_bar_length);
+      ctx.stroke();
+      ctx.closePath();
+    }
+    //y 축 위 가로 눈금
+    for(let i = 0; i<100; i++){
+      ctx.beginPath();
+      ctx.strokeStyle="black";
+      ctx.moveTo(origin_X-grid_short_bar_length,origin_Y-(i-30)*y_grid_short_bar_space);
+      ctx.lineTo(origin_X+grid_short_bar_length,origin_Y-(i-30)*y_grid_short_bar_space);
+      ctx.stroke();
+      ctx.closePath();
+    }
+
+    //x 축 위 세로 grid
+    for(let i = 0; i<100; i++){
+      ctx.beginPath();
+      ctx.strokeStyle="black";
+      ctx.moveTo(origin_X+(i-30)*x_grid_short_bar_space,canvas.height);
+      ctx.lineTo(origin_X+(i-30)*x_grid_short_bar_space,0);
+      ctx.stroke();
+      ctx.closePath();
+    }
+    //y 축 위 가로 grid
+    for(let i = 0; i<100; i++){
+      ctx.beginPath();
+      ctx.strokeStyle="#778855";
+      ctx.moveTo(0,origin_Y-(i-30)*y_grid_short_bar_space);
+      ctx.lineTo(canvas.width,origin_Y-(i-30)*y_grid_short_bar_space);
+      ctx.stroke();
+      ctx.closePath();
+    }
   }
-  //y 축 위 가로 눈금
-  for(let i = 0; i<100; i++){
-    ctx.beginPath();
-    ctx.strokeStyle="black";
-    ctx.moveTo(origin_X-grid_short_bar_length,origin_Y-(i-30)*y_grid_short_bar_space);
-    ctx.lineTo(origin_X+grid_short_bar_length,origin_Y-(i-30)*y_grid_short_bar_space);
-    ctx.stroke();
-    ctx.closePath();
-  }
+  else(option_gridSpace === "SPACEDEPEND")
+  {
+    let X_START = value1;
+    let X_END = value2;
+    let X_STEP_COUNT = X_END;
+        
+    let Y_START = 0;
+    let Y_END = 10;
+    let Y_STEP_COUNT = 10;
+    let X_START_COORDINATES = origin_X;
+    let Y_START_COORDINATES = origin_Y;
 
-  //x 축 위 세로 grid
-  for(let i = 0; i<100; i++){
-    ctx.beginPath();
-    ctx.strokeStyle="black";
-    ctx.moveTo(origin_X+(i-30)*x_grid_short_bar_space,canvas.height);
-    ctx.lineTo(origin_X+(i-30)*y_grid_short_bar_space,0);
-    ctx.stroke();
-    ctx.closePath();
+    let X_END_COORDINATES = canvas.width - 20;
+    let Y_END_COORDINATES = 20;
+
+    x_grid_short_bar_space = (X_END_COORDINATES - X_START_COORDINATES)/X_STEP_COUNT;
+    y_grid_short_bar_space = (Y_START_COORDINATES - Y_END_COORDINATES)/Y_STEP_COUNT;
+
+    //x 축 위 세로 눈금
+    for(let i = 0; i<X_STEP_COUNT+3; i++){
+      ctx.beginPath();
+      ctx.strokeStyle="black";
+      ctx.moveTo(origin_X+(i-3)*x_grid_short_bar_space,origin_Y-grid_short_bar_length);
+      ctx.lineTo(origin_X+(i-3)*x_grid_short_bar_space,origin_Y+grid_short_bar_length);
+      ctx.stroke();
+      ctx.closePath();
+    }
+    //y 축 위 가로 눈금
+    for(let i = 0; i<Y_STEP_COUNT+3; i++){
+      ctx.beginPath();
+      ctx.strokeStyle="red";
+      ctx.moveTo(origin_X-grid_short_bar_length,origin_Y-(i-3)*y_grid_short_bar_space);
+      ctx.lineTo(origin_X+grid_short_bar_length,origin_Y-(i-3)*y_grid_short_bar_space);
+      ctx.stroke();
+      ctx.closePath();
+    }
+
+    //x 축 위 세로 grid
+    for(let i = 0; i<X_STEP_COUNT+3; i++){
+      ctx.beginPath();
+      ctx.strokeStyle="green";
+      ctx.moveTo(origin_X+(i-3)*x_grid_short_bar_space,canvas.height);
+      ctx.lineTo(origin_X+(i-3)*x_grid_short_bar_space,0);
+      ctx.stroke();
+      ctx.closePath();
+    }
+    //y 축 위 가로 grid
+    for(let i = 0; i<Y_STEP_COUNT+3; i++){
+      ctx.beginPath();
+      ctx.strokeStyle="#778855";
+      ctx.moveTo(0,origin_Y-(i-3)*y_grid_short_bar_space);
+      ctx.lineTo(canvas.width,origin_Y-(i-3)*y_grid_short_bar_space);
+      ctx.stroke();
+      ctx.closePath();
+    }
   }
-  //y 축 위 가로 grid
-  for(let i = 0; i<100; i++){
-    ctx.beginPath();
-    ctx.strokeStyle="#778855";
-    ctx.moveTo(0,origin_Y-(i-30)*y_grid_short_bar_space);
-    ctx.lineTo(canvas.width,origin_Y-(i-30)*y_grid_short_bar_space);
-    ctx.stroke();
-    ctx.closePath();
-  }
+}
+
+  
 
 // const graph1 = new graph(1,1);
 // graph.draw();
@@ -112,17 +209,11 @@ let Ydata_count = Math.floor(Ydata_range - Ydata_step);
 
 /*
 //draw grid
-Xgrid_start = 20;
-Xgrid_end = canvas.width-20;
-Ygrid_start = 20;
-Ygrid_end = canvas.height - 20;
+let Xgrid_start = 20;
+let Xgrid_number = 50;
+let Ygrid_start = 20;
+let Ygrid_number = 30;
 
-<<<<<<< HEAD
-ctx.beginPath();
-ctx.moveTo(20,canvas.height - 20)
-ctx.lineTo(20,20);
-ctx.stroke();*/
-=======
 let Xgrid_end = canvas.width - 20;
 let Xgrid_width = Xgrid_end - Xgrid_start;
 let Xgrid_space = Xgrid_width / Xgrid_number;
@@ -147,33 +238,27 @@ for (let j = 0; j < Ygrid_number+1; j++) {
 
 function drawNumberBox(x,y,value,color){
   const value_string = (value).toString();
-  let box_X = origin_X + x*x_grid_short_bar_space;
-  let box_Y = origin_Y - y*y_grid_short_bar_space;
-  let box_X_width = grid_short_bar_space*0.8;
-  let box_Y_height = grid_short_bar_space*0.8;
-  let box_X_LeftTop = box_X-0.5*box_X_width;
-  let box_Y_LeftTop = box_Y-0.5*box_Y_height;
-  let box_Y_LeftBottom = box_Y+0.5*box_Y_height;
-  
-  let colors = ["#E97132","#196B24","#0F9ED5","#A02B93","#4EA72E","#156082"];
-  ctx.fillStyle = colors[value%6];
-  ctx.strokeStyle = colors[value%6];
-  ctx.font = "bold 18px serif";
-  ctx.textAlign = "center";
-  ctx.textBaseline = "middle";
-  ctx.fillRect(box_X_LeftTop, box_Y_LeftTop, box_X_width, box_Y_height);
-  ctx.strokeRect(box_X_LeftTop, box_Y_LeftTop, box_X_width, box_Y_height);
-  ctx.fillStyle = "black";
-  ctx.fillText(value_string,box_X,box_Y);
+    let box_X = origin_X + x*x_grid_short_bar_space;
+    let box_Y = origin_Y - y*y_grid_short_bar_space;
+    let box_X_width = x_grid_short_bar_space*0.8;
+    let box_Y_height = y_grid_short_bar_space*0.8;
+    let box_X_LeftTop = box_X-0.5*box_X_width;
+    let box_Y_LeftTop = box_Y-0.5*box_Y_height;
+    let box_Y_LeftBottom = box_Y+0.5*box_Y_height;
+    
+    let colors = ["#E97132","#196B24","#0F9ED5","#A02B93","#4EA72E","#156082"];
+    ctx.fillStyle = colors[value%6];
+    ctx.strokeStyle = colors[value%6];
+    ctx.font = "bold 18px serif";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillRect(box_X_LeftTop, box_Y_LeftTop, box_X_width, box_Y_height);
+    ctx.strokeRect(box_X_LeftTop, box_Y_LeftTop, box_X_width, box_Y_height);
+    ctx.fillStyle = "black";
+    ctx.fillText(value_string,box_X,box_Y);
 }
 
-function drawNumberBoxes(){
-  let value1 = document.getElementById('text1').value;
-  let value2 = document.getElementById('text2').value;
-
-  console.log(value1);
-  console.log(value2);  
-
+function drawNumberBoxes(value1, value2){
   for(let i=Number(value1); i<Number(value2);i++){
     //console.log(printPrimeList(i));
     let primeList = printPrimeList(i)
@@ -192,14 +277,22 @@ function drawNumberBoxes(){
 
 
 
-drawGraph();
-drawNumberBoxes();
+drawGraph(value1,value2);
+drawNumberBoxes(value1,value2);
 
 document.getElementById('submit').onclick = function() {
   clearCanvas();
-  drawGraph();
   let value1 = document.getElementById('text1').value;
   let value2 = document.getElementById('text2').value;
+  drawGraph(value1,value2);
+  drawNumberBoxes(value1,value2);
+};
+
+document.getElementById('text2').oninput = function() {
+  clearCanvas();
+  let value1 = document.getElementById('text1').value;
+  let value2 = document.getElementById('text2').value;
+  drawGraph(value1,value2);
   drawNumberBoxes(value1,value2);
 };
 
@@ -219,4 +312,3 @@ for(nn=2;nn<16;nn++){
   console.log(isPrime(nn));
 }
 */
->>>>>>> parent of 9e03b87 (소수 박스 첫 완성!)
